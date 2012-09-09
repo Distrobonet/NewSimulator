182c0b7de865ae6a0cf3b8fd7d725ad0fda4381a
breakspirit <breakspirit@gmail.com>
2012-09-09 03:24:45 +0000

    Almost everything needed for the Formation class with single function formations is implemented.  Still needs some loose ends tied to work with other classes.

--- include/Simulator/Formation.h
+++ include/Simulator/Formation.h
@@ -1,36 +1,78 @@
 
 #ifndef FORMATION_H_
 #define FORMATION_H_
-#include <Simulator/PhysicsVector.h>
+#include <vector>
+#include "Simulator/PhysicsVector.h"
 
 using namespace std;
 
 typedef float (*Function)(const float);
 
+// global constants
+static const int DEFAULT_SEED_ID = 3;		// Hard-coded seed ID because it is not selectable
 
-class Formation {
+static const Function DEFAULT_FORMATION_FUNCTION = NULL;
+static const float  DEFAULT_FORMATION_RADIUS   = 1.0f;
+static const double X_ROOT_THRESHOLD           = 5E-7;
+static const int    X_N_ITERATIONS             = 100;
+
+
+//class Formation : protected vector<Function>			// Multi-function formations will use this line eventually
+class Formation
+{
 	public:
 		Formation();
 		Formation(const float radius, const PhysicsVector frp,
-				const int formationID, const float formationRelativeOrientation);
+				  const int formationID, const float formationRelativeOrientation);
 		Formation(const Formation &f);
 		virtual ~Formation();
-		Formation& operator=(const Formation &f);
+		Formation& operator=(const Formation &newFormation);
 
-		int getFormation();
-		void setFormationID(int formationID);
+		// Mutators
+		void setFunction(const Function newFunction = DEFAULT_FORMATION_FUNCTION);
+		void setFormationID(int newFormationID);
+		void setRadius(int newRadius);
+		void setFormationRelativeOrientation(float newFormationRelativeOrientation);
+		void setSeedFormationRelativePosition(PhysicsVector newFormationRelativePosition);
+
+
+		// Accessors
+		Function getFunction();
+		int getFormationID();
 		int getRadius();
-		void setRadius(int radius);
 		float getFormationRelativeOrientation();
-		void setFormationRelativeOrientation(float fro);
 		PhysicsVector getSeedFormationRelativePosition();
-		void setSeedFormationRelativePosition(PhysicsVector frp);
+		int getSeedID();
+		vector<PhysicsVector> getRelationships(const PhysicsVector someVector);
 
+		PhysicsVector getRelationship( const Function intersectingFunction = DEFAULT_FORMATION_FUNCTION,
+		                               const float intersectingCircleRadius = DEFAULT_FORMATION_RADIUS,
+		                               const PhysicsVector centerPosition = PhysicsVector(),
+		                               const float  rotationOfRelationship = 0.0f);
+
+		PhysicsVector getRelationship( const int   positionOfDesiredFunction   = 0,
+		                       	   	   const float intersectingCircleRadius     = DEFAULT_FORMATION_RADIUS,
+		                       	   	   const PhysicsVector  centerPosition     = PhysicsVector(),
+		                       	   	   const float rotationOfRelationship = 0.0f);
+
+		float getFunctionIntersection(const Function formationToCopy = DEFAULT_FORMATION_FUNCTION,
+		                 	 	 	  const float  intersectingCircleRadius = DEFAULT_FORMATION_RADIUS,
+		                 	 	 	  const PhysicsVector centerPosition = PhysicsVector(),
+		                 	 	 	  const float offset = 0.0f);
+
+
+
+		// Member variables
+		int seedID;
 		Formation fx(int formationId);
+		Function currentFunction;
 		int formationID;									// The identifier for the current formation function
 		int radius;											// The desired distance between neighbors
 		float formationRelativeOrientation;					// Orientation of each robot relative to the formation
 		PhysicsVector seedFormationRelativePosition;		// FRP that serves as starting point from which formation and relationships will propagate
+
+
+
 
 
 		// <test formation functions>
--- src/Formation.cpp
+++ src/Formation.cpp
@@ -6,8 +6,10 @@ using namespace std;
 
 Formation::Formation()
 {
+	seedID = DEFAULT_SEED_ID;
 	setRadius(1);
 	setFormationID(0);
+//	setSeedFormationRelativePosition();
 	setFormationRelativeOrientation(0);
 
 	seedFormationRelativePosition.x = 0.0f;
@@ -15,42 +17,56 @@ Formation::Formation()
 	seedFormationRelativePosition.z = 0.0f;
 }
 
-Formation::Formation(const float radius, const PhysicsVector frp,
-		const int formationID, const float formationRelativeOrientation)
+Formation::Formation(const float radius, const PhysicsVector frp, const int formationID, const float formationRelativeOrientation)
 {
-	setFormationID(formationID);
+	seedID = DEFAULT_SEED_ID;
 	setRadius(radius);
-	setSeedFormationRelativePosition(frp);
 	setFormationID(formationID);
+	setSeedFormationRelativePosition(frp);
 	setFormationRelativeOrientation(formationRelativeOrientation);
 }
 
-Formation::Formation(const Formation &f) {
-	*this = f;
+// Copy constructor
+Formation::Formation(const Formation &newFormation)
+{
+	*this = newFormation;
 }
 
-Formation& Formation::operator=(const Formation &f) {
-	radius = f.radius;
-	formationID = f.formationID;
-	formationRelativeOrientation = f.formationRelativeOrientation;
-	setSeedFormationRelativePosition(f.seedFormationRelativePosition);
+Formation& Formation::operator=(const Formation &newFormation)
+{
+	radius = newFormation.radius;
+	formationID = newFormation.formationID;
+	formationRelativeOrientation = newFormation.formationRelativeOrientation;
+	setSeedFormationRelativePosition(newFormation.seedFormationRelativePosition);
 
 	return *this;
 }
 
 Formation::~Formation()
 {
-
 }
 
-int Formation::getFormation()
+void Formation::setFunction(const Function newFunction)
+{
+	//clear();						// Clears this vector if functions, should be used when we switch to multifunction.
+	//this->push_back(newFunction);	// Adds the new function to this vector.  Will replace the next line.
+
+	currentFunction = newFunction;
+}
+
+Function Formation::getFunction()
+{
+	return currentFunction;
+}
+
+int Formation::getFormationID()
 {
 	return formationID;
 }
 
-void Formation::setFormationID(int formationID)
+void Formation::setFormationID(int newFormationID)
 {
-	this->formationID = formationID;
+	formationID = newFormationID;
 }
 
 int Formation::getRadius()
@@ -68,27 +84,108 @@ float Formation::getFormationRelativeOrientation()
 	return formationRelativeOrientation;
 }
 
-void Formation::setFormationRelativeOrientation(float fro)
+void Formation::setFormationRelativeOrientation(float newFormationRelativeOrientation)
 {
-	formationRelativeOrientation = fro;
+	formationRelativeOrientation = newFormationRelativeOrientation;
 }
 PhysicsVector Formation::getSeedFormationRelativePosition()
 {
 	return seedFormationRelativePosition;
 }
 
-void Formation::setSeedFormationRelativePosition(PhysicsVector frp)
+void Formation::setSeedFormationRelativePosition(PhysicsVector newFormationRelativePosition)
 {
-	seedFormationRelativePosition.x = frp.x;
-	seedFormationRelativePosition.y = frp.y;
-	seedFormationRelativePosition.z = frp.z;
+	seedFormationRelativePosition.x = newFormationRelativePosition.x;
+	seedFormationRelativePosition.y = newFormationRelativePosition.y;
+	seedFormationRelativePosition.z = newFormationRelativePosition.z;
 
     for (int i = 0; i < 3; ++i)
     {
-    	seedFormationRelativePosition.translate[i]  = frp.translate[i];
-    	seedFormationRelativePosition.rotate[i]     = frp.rotate[i];
-    	seedFormationRelativePosition. scale[i]     = frp.scale[i];
+    	seedFormationRelativePosition.translate[i]  = newFormationRelativePosition.translate[i];
+    	seedFormationRelativePosition.rotate[i]     = newFormationRelativePosition.rotate[i];
+    	seedFormationRelativePosition. scale[i]     = newFormationRelativePosition.scale[i];
     }
-    seedFormationRelativePosition. showLine         = frp.showLine;
-    seedFormationRelativePosition.showHead          = frp.showHead;
+    seedFormationRelativePosition. showLine         = newFormationRelativePosition.showLine;
+    seedFormationRelativePosition.showHead          = newFormationRelativePosition.showHead;
+}
+
+int Formation::getSeedID()
+{
+	return seedID;
+}
+
+// This function is useful when multi-function is implemented
+vector<PhysicsVector> Formation::getRelationships(const PhysicsVector c)
+{
+	vector<PhysicsVector> temp;	// See below \/
+	return temp;				// Temporary, just to remove warning.  Remove when implementing multi-function
+
+//    if (isEmpty())
+//    	return vector<PhysicsVector>();
+//
+//    vector<PhysicsVector> rels;
+//    Function           curr = NULL;
+//    for (int i = 0; i < getSize(); ++i)
+//    {
+//        if (!getHead(curr))
+//        	break;
+//
+//        rels.insertTail(getRelationship(curr, -radius, c, heading));
+//        rels.insertTail(getRelationship(curr,  radius, c, heading));
+//        ++(*this);
+//    }
+//    return rels;
+}   // getRelationships(const Vector)
+
+
+// Uses the secant method to calculate the intersection of the function and a circle centered at
+// the parameterized vector position centerPosition with the appropriate radius, returning a vector
+// from centerPosition to this intersection.
+// Uses the secant method:    x_(n + 1) = x_n - f(x_n) * (x_n - x_(n - 1)) / (f(x_n) - f(x_(n - 1))).
+PhysicsVector Formation::getRelationship(const Function intersectingFunction, const float intersectingCircleRadius,
+		                      	  	  	 const PhysicsVector centerPosition, const float rotationOfRelationship)
+{
+    if (intersectingFunction == NULL)
+    	return PhysicsVector();
+
+    float xn = centerPosition.x + intersectingCircleRadius + X_ROOT_THRESHOLD;
+    float xn_1 = centerPosition.x + intersectingCircleRadius - X_ROOT_THRESHOLD;
+    float intersect = 0.0f;
+    float error = 0.0f;
+
+    for (int i = 0; i < X_N_ITERATIONS; ++i)
+    {
+        intersect     = getFunctionIntersection(intersectingFunction, intersectingCircleRadius, centerPosition, xn);
+        error         = intersect * (xn - xn_1) /
+                       (intersect - getFunctionIntersection(intersectingFunction, intersectingCircleRadius, centerPosition, xn_1));
+        if (abs(error) <= X_ROOT_THRESHOLD) break;
+        xn_1          = xn;
+        xn           -= error;
+    }
+
+    PhysicsVector relationship;
+
+    // todo: These lines SHOULD work.  Must figure out why they don't.
+    relationship =  PhysicsVector(xn, intersectingFunction(xn)) - centerPosition;
+    relationship.rotateRelative(-rotationOfRelationship);
+    return relationship;
+}
+
+
+// Useful when we implement multi-function
+PhysicsVector Formation::getRelationship(const int positionOfDesiredFunction, const float intersectingCircleRadius,
+		                      const PhysicsVector  centerPosition, const float rotationOfRelationship)
+{
+	PhysicsVector temp;	// See below \/
+	return temp;		// Temporary, just to remove warning.  Remove when implementing multi-function
+
+	// This will be used when multi-function is implemented
+	//return getRelationship(getFunction(positionOfDesiredFunction), intersectingCircleRadius, centerPosition, rotationOfRelationship);
+}
+
+
+float Formation::getFunctionIntersection(const Function formationToCopy, const float  intersectingCircleRadius,
+										 const PhysicsVector centerPosition, const float offset)
+{
+	return pow(offset - centerPosition.x, 2.0f) + pow(formationToCopy(offset) - centerPosition.y, 2.0f) - pow(intersectingCircleRadius, 2.0f);
 }
